85de4f88bb2892458d2a3255af475a3bb7eb9f0e
darren <blountdj@gmail.com>
2024-02-17 11:22:29 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+Website created following this tutorial:
+
+https://www.youtube.com/watch?v=LMagNcngvcU&list=PL6QREj8te1P6CkO_4OIK1-nwG5OxCD5tR&index=2
+
+Libraries used:
+
+- react-icons
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
